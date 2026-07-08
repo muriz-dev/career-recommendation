055612af1713c8d495a564c922cd9dc2724ee1b0
muriz.dev <mhmdrizki.dev@gmail.com>
2026-07-08 16:42:39 +0700

    add punk_tab to preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,6 +8,7 @@ from nltk.stem import WordNetLemmatizer
 
 # Download resources
 nltk.download("punkt", quiet=True)
+nltk.download("punkt_tab", quiet=True)
 nltk.download("stopwords", quiet=True)
 nltk.download("wordnet", quiet=True)
 nltk.download("omw-1.4", quiet=True)
